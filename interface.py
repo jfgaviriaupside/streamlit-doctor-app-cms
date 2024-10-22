c0819cf8e00dd576e7b5ace48f8cdb740330e510
jfgaviriaupside <juan.gaviria@upside.com.co>
2024-10-22 13:32:08 -0500

    Update interface.py

--- interface.py
+++ interface.py
@@ -28,8 +28,8 @@ if 'current_page' not in st.session_state:
 
 if check_password():
     # Load the data (use your own file path)
-    doctor_matching_df = pd.read_excel('Doctor_Matching_With_Procedures_Separate_Sheets.xlsx', sheet_name='Doctor_Matching')
-    procedure_prioritization_df = pd.read_excel('Doctor_Matching_With_Procedures_Separate_Sheets.xlsx', sheet_name='Procedure_Prioritization')
+    doctor_matching_df = pd.read_excel('Doctor_Matching_With_Procedures_Separate_Sheets_V2.xlsx', sheet_name='Doctor_Matching')
+    procedure_prioritization_df = pd.read_excel('Doctor_Matching_With_Procedures_Separate_Sheets_V2.xlsx', sheet_name='Procedure_Prioritization')
     insurance_payments_df = pd.read_excel('Insurace_Payments.xlsx', sheet_name='Insurance Payment Avgs')
 
     # Clean the data by filling NaN values in relevant columns
