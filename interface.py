4d838b92df2c9de9b893fb089e5607d75ff85057
jfgaviriaupside <juan.gaviria@upside.com.co>
2024-10-24 10:30:09 -0500

    Update interface.py

--- interface.py
+++ interface.py
@@ -80,7 +80,13 @@ if check_password():
                 (procedure_prioritization_df['Prioritization Index Procedure'].notna())
             ].sort_values(by='Prioritization Index Procedure', ascending=False).drop_duplicates(subset='Referring Physician').reset_index(drop=True)
             filtered_procedures['Rank'] = filtered_procedures.index + 1
-            filtered_procedures = filtered_procedures.merge(doctor_matching_df[['Referring Physician', 'Luis, Gerardo o Alex', 'CAGR']].drop_duplicates(subset='Referring Physician'), on='Referring Physician', how='left')
+
+            # Add CAGR column if it exists in doctor_matching_df
+            if 'CAGR' in doctor_matching_df.columns:
+                filtered_procedures = filtered_procedures.merge(doctor_matching_df[['Referring Physician', 'Luis, Gerardo o Alex', 'CAGR']].drop_duplicates(subset='Referring Physician'), on='Referring Physician', how='left')
+            else:
+                filtered_procedures['CAGR'] = 'Not Available'
+
             filtered_procedures['Luis, Gerardo o Alex'] = filtered_procedures['Luis, Gerardo o Alex'].apply(lambda x: f"YES, {x}" if x else "NO")
             filtered_procedures = filtered_procedures[['Rank', 'Referring Physician', 'Procedure', 'Referrals', 'CAGR', 'Luis, Gerardo o Alex']]
 
